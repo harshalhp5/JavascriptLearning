1f36321b3606c83f7537db5620a6ac71749ca8e2
harshalhp5 <63289664+harshalhp5@users.noreply.github.com>
2020-07-11 03:15:07 +0530

    Night changes

--- js/script.js
+++ js/script.js
@@ -9,6 +9,16 @@ function greet (){
 greet();
 
 
+
+//function using parameter and argument
+function greet1(name,lastname)
+{
+    console.log('Hello ' + name +' ' + lastname );
+}
+greet1('Harshal1' , 'Pohare');
+greet1('Rupesh' , '');
+
+
 // Playing with HTML
 //let result = document.getElementById("result");
 function validateEmail(email){
@@ -24,10 +34,43 @@ function validateEmail(email){
 
 console.log(validateEmail('hp@gmail.com'));
 
-//function using parameter and argument
-function greet1(name,lastname)
-{
-    console.log('Hello ' + name +' ' + lastname );
+
+// Javascript strings
+
+const message = 'This is a string';
+const message1 = 'This is how we put \' in JS and \n breaking line';
+
+//String using Template
+
+const message2 = `This needs no \ backslash to define 'single quotes' we can directly use it.
+I simply pressed enter and Voila we are on next line`;
+
+// Template with PLACEHOLDERS !!
+
+const name1 ='Harshal';
+const message3 = 'Hi ' + name1 + '\n So glad you\'re actually learning this.' + '\n Regards,' + '\n Yourself';
+const another =
+`Hi ${name1},
+
+So glad you're actually learning this.
+
+Regards,
+Yourself`;
+
+// LOOOOOOOOOOPSSSSS !!
+
+for(let i=0; i<5; i++) {
+    console.log('loooooop check' , i);
 }
-greet1('Harshal1' , 'Pohare');
-greet1('Rupesh' , '');
+ // Using Loop to Display Odd & Even numbers
+
+console.log('These are Odd Number till 10');
+for(let i=1; i<=10; i++) {    
+     if(i % 2 !== 0) console.log(i);
+
+}
+
+console.log('These are Even Number till 10');
+for(i=1; i<=10; i++) {
+      if(i % 2 == 0) console.log(i);
+}
