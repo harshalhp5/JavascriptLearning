2ecac46639300d733d87cd617c0db61b30171b36
harshalhp5 <63289664+harshalhp5@users.noreply.github.com>
2020-07-10 14:49:28 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//array
+let selectedcolors = ['red', 'blue','yellow'];
+let name = 'Rupesh';
+
+//function
+function greet (){
+    console.log('Hello Harshal');
+}
+greet();
+
+
+// Playing with HTML
+//let result = document.getElementById("result");
+function validateEmail(email){
+    if(email.includes('@')){
+        if(email.includes('.')){
+            //result.innerText = true;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log(validateEmail('hp@gmail.com'));
+
+//function using parameter and argument
+function greet1(name,lastname)
+{
+    console.log('Hello ' + name +' ' + lastname );
+}
+greet1('Harshal1' , 'Pohare');
+greet1('Rupesh' , '');
